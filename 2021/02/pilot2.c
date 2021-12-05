8e3b3c0b7567dd12d451a671256bc112394e2ff9
Javier López <graulopezjavier@gmail.com>
2021-12-05 09:11:52 -0500

    day2 boyyyyyy, I'm ccinggg

---
+++ 2021/02/pilot2.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+  FILE *fp = fopen("input", "r");
+  char dir;
+  int mag;
+  int pos[3] = {0, 0, 0};
+  int count = 0;
+  // Consume all left whitespace, consume 1 char, consume a non captured
+  // string, consume a dicgit (which also consumes the leading whitespace).
+  while (fscanf(fp, " %1c%*s%d", &dir, &mag) != EOF) {
+    switch (dir) {
+    case 'f':
+      pos[0] += mag;
+      pos[1] += pos[2] * mag;
+      break;
+    case 'u':
+      pos[2] -= mag;
+      break;
+    case 'd':
+      pos[2] += mag;
+      break;
+    default:
+      printf("Invalid direction: %c\n", dir);
+      exit(1);
+    }
+  }
+  printf("Final position x:%i, y:%i a:%i, x * y = %i\n", pos[0], pos[1], pos[2], pos[0] * pos[1]);
+  return 0;
+}
