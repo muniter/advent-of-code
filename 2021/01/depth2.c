71def716005f80b55383f7712c8fe0aa9d2a8d19
Javier López <graulopezjavier@gmail.com>
2021-12-04 22:32:08 -0500

    less branches, no trees :)

--- 2021/01/depth2.c
+++ 2021/01/depth2.c
@@ -3,12 +3,8 @@
 #define SIZE 4
 int ind(int i)
 {
-    if (i < 0)
-        return i + SIZE;
-    else if (i >= SIZE)
-        return i - SIZE;
-    else
-        return i;
+  return abs(SIZE * (i < 0) + i - SIZE * (i >= SIZE));
+  // return (SIZE + (i % SIZE)) % SIZE;
 }
 
 int main (int argc, char *argv[])
@@ -23,27 +19,17 @@ int main (int argc, char *argv[])
   int slider = 0;
   int count = 0;
   int ok = 0;
-  while (1) {
-    count++;
-    ok = count >= SIZE;
-    if (fgets(charint, 10, fp) == NULL) {
-      printf("At the end the counts %d\n", count);
-      printf("Increments: %d, decrements: %d\n", increments, decrements);
-      break;
-    }
+  for (count = 0; fgets(charint, 10, fp) != NULL; count++) {
+    ok = count > SIZE;
     values[slider] = atoi(charint);
-    // TODO: I WANT NO BRANCHES1!!!!!!!
-    if (ok) {
-      // Print each of the previous used indexes
-      printf("%d %d %d %d\n", slider, ind(slider - 1), ind(slider - 2), ind(slider - 3));
-      current = values[slider] + values[ind(slider - 1)] + values[ind(slider - 2)];
-      previous = values[ind(slider - 1)] + values[ind(slider - 2)] + values[ind(slider - 3)];
-      increments += ok * current > previous;
-      decrements += ok * previous > current;
-      printf("Window: %d %d %d %d pre: %d, curr: %d\n", values[0], values[1], values[2], values[3], previous, current);
-    }
+    current = values[slider] + values[ind(slider - 1)] + values[ind(slider - 2)];
+    previous = values[ind(slider - 1)] + values[ind(slider - 2)] + values[ind(slider - 3)];
+    increments += ok * current > previous;
+    decrements += ok * current > previous;
     slider++;
     slider -= (slider == SIZE) * SIZE;
   }
+  printf("At the end the counts %d\n", count);
+  printf("Increments: %d, decrements: %d\n", increments, decrements);
   return 0;
 }
